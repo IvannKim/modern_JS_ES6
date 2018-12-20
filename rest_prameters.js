f88ad14a602e89a75c3f8c17c4716369ce931f66
Peter Kim <seongjooon@gmail.com>
2018-12-20 14:59:10 +0900

    rest_parameters

---
+++ rest_prameters.js
@@ -0,0 +1,16 @@
+//가짜 배열인 arguments를 진빠 배열로 치환
+function checkNum() {
+    const argArray = Array.prototype.slice.call(arguments);
+    console.log(argArray);
+    const result = argArray.every(v => typeof v === 'number')
+    console.log(result)
+}
+
+const result = checkNum(10,2,3,4,5,false)
+
+function checkNum1(...argArray) {
+    const result = argArray.every(v => typeof v === 'number')
+    console.log(result)
+}
+
+const result1 = checkNum1(10,2,3,4,5)
